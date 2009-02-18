b87205f8bb5e5a2b588c52533443492789f9d1f0
gnu.andrew.rocks <gnu.andrew.rocks@d87e18c9-e728-0410-93b3-a5baa3e6ab00>
2009-02-18 03:21:21 +0000

    Update license on existing files to Affero GPLv3.

--- trunk/src/uk/ac/shef/dcs/dynamite/lts/Label.java
+++ trunk/src/uk/ac/shef/dcs/dynamite/lts/Label.java
@@ -1,29 +1,26 @@
 /* Label.java - Represents a transition label.
-   Copyright (C) 2007 The University of Sheffield
-
-This file is part of DynamiTE.
-
-DynamiTE is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-DynamiTE is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with DynamiTE; see the file COPYING.  If not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301 USA.
-
-Linking this library statically or dynamically with other modules is
-making a combined work based on this library.  Thus, the terms and
-conditions of the GNU General Public License cover the whole
-combination.
-*/
-
+ * Copyright (C) 2007 The University of Sheffield
+ *
+ * This file is part of DynamiTE.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ */
 package uk.ac.shef.dcs.dynamite.lts;
 
 /**
